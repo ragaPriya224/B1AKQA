9874f46bbf7e055a313e3693688ff56df1be27de
Priya <aaa@gmail.com>
2022-09-02 11:20:12 +0530

    supplier example

---
+++ java8/code/B1Akqa/src/java8/day2/FunctionDemo.java
@@ -0,0 +1,14 @@
+package java8.day2;
+
+import java.util.function.Function;
+
+public class FunctionDemo {
+
+	public static void main(String[] args) {
+		
+		Function<String, Integer> functionDemo = name -> name.length();
+		System.out.println(functionDemo.apply("hello"));
+
+	}
+
+}
