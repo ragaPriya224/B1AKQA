3eb38679039f775e5e81456c76e1489146c6a969
Priya <aaa@gmail.com>
2022-09-02 15:31:01 +0530

    biconsumer person hobby demo

--- java8/code/B1Akqa/src/java8/day2/BiConsumerPersonDemo.java
+++ java8/code/B1Akqa/src/java8/day2/BiConsumerPersonDemo.java
@@ -10,12 +10,24 @@ public class BiConsumerPersonDemo {
 
 	static List<Person> personList = PersonRepository.getAllPersons();
 	static BiConsumer<String, Double> income = (name,salary) ->
-	System.out.println("name is: "+name + "   salary is "+ salary);	
+	System.out.println("name is: "+name + "   salary is "+ salary);
+	
+	static BiConsumer<String,List<String>> hobbyConsumer = (name,hobbies) ->
+	 				System.out.println("name is:  "+name +"    hobbies are  " +hobbies);
 
+	
 	public static void main(String[] args) {
-		fetchSalary();
+//		fetchSalary();
+		fetchHobbies();
 	}
 	
+	private static void fetchHobbies() {
+		
+		personList.forEach(per->{
+			hobbyConsumer.accept(per.getName(),per.getHobbies());
+		});
+	}
+
 	//name and salary 
 	private static void fetchSalary() {
 		personList.forEach(per->{
